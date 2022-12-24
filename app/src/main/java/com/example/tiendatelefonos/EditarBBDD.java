1f9a0aaf509f88f5588c12c94b71311cd850742a
David Patron Fernandez <tronez98@gmail.com>
2022-12-24 17:54:04 +0100

    24/12

--- app/src/main/java/com/example/tiendatelefonos/EditarBBDD.java
+++ app/src/main/java/com/example/tiendatelefonos/EditarBBDD.java
@@ -1,6 +1,9 @@
 package com.example.tiendatelefonos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,12 @@ public class EditarBBDD extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_bbdd);
+    }
+
+    public void onClickAceptar(View v) {
+        Intent i = new Intent(this, MainActivity.class);
+        Toast toast = Toast.makeText(this, "hola", Toast.LENGTH_SHORT);
+        toast.show();
+        startActivity(i);
     }
 }
